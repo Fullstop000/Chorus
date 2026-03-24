512524e1763f657870b2d6dabdeee6c4e39d53b8
Fullstop000 <fullstop1005@gmail.com>
2026-03-24 14:55:40 +0800

    refactor(models): delete empty models.rs stub

--- src/models.rs
+++
@@ -1,3 +0,0 @@
-// This file has been emptied as part of the models distribution refactor.
-// All types have been moved to their owning store/activity_log modules.
-// This file will be deleted once the refactor is complete.
