4af4b45ddb08d5b4068c4ca5a6bbb1496384fdcb
Fullstop000 <fullstop1005@gmail.com>
2026-03-31 09:45:21 +0800

    refactor(ui): migrate CreateChannelModal to shadcn Dialog + React Hook Form

--- ui/src/components/Sidebar.tsx
+++ ui/src/components/Sidebar.tsx
@@ -4,7 +4,7 @@ import { useApp } from '../store'
 import type { AgentInfo, ChannelInfo } from '../types'
 import { isVisibleSidebarChannel } from '../sidebarChannels'
 import { CreateAgentModal } from './CreateAgentModal'
-import { CreateChannelModal } from './CreateChannelModal'
+import { CreateChannelModal } from './Channels/CreateChannelModal'
 import { DeleteChannelModal, EditChannelModal } from './EditChannelModal'
 import './Sidebar.css'
 
@@ -368,7 +368,8 @@ export function Sidebar() {
       {showCreateChannel && (
         <CreateChannelModal
           defaultMode={createModalMode}
-          onClose={() => setShowCreateChannel(false)}
+          open={showCreateChannel}
+          onOpenChange={(open) => setShowCreateChannel(open)}
           onCreated={(created) => {
             setShowCreateChannel(false)
             setSelectedChannel(`#${created.name}`, created.id ?? null)
